3345f61f258ebc1a548514718caa6cb56e218b73
Manuelbailey101 <115514197+Manuelbailey101@users.noreply.github.com>
2022-11-30 17:15:25 -0800

    Task Manager - First Commit

---
+++ styles/scripts/task.js
@@ -0,0 +1,18 @@
+
+
+class Task {
+    constructor(important,title,desc,dueDate,category,priority,cost) {
+       this.important = important;
+       this.title = title;
+       this.discription = desc;
+       this.dueDate = dueDate;
+       this.category = category;
+       this.priority = priority;
+       this.cost = cost;
+    }
+
+
+}
+
+
+
